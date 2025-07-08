3b7c67b357621323954acdcae4858927c188379f
ridwan bahtiar <ridwanbahtiar15@gmail.com>
2025-07-09 06:13:33 +0700

    fix code

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-// file: server.js
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
 let latestState = {};
+
+// cors
+app.use(
+  cors({
+    origin: "*",
+    methods: ["POST", "PATCH", "DELETE"],
+  })
+);
 
 app.post("/api/update", (req, res) => {
   latestState = req.body;
@@ -24,7 +32,7 @@ app.get("/api/status", (req, res) => {
 app.post("/api/control", async (req, res) => {
   console.log(req.body);
   try {
-    const response = await axios.post("http://192.168.0.103:80/set", req.body); // IP ESP
+    const response = await axios.post(`${process.env.ESP_HOST}/set`, req.body); // IP ESP
     res.json({ success: true, result: response.data });
   } catch (e) {
     console.error(e.message);
@@ -33,5 +41,5 @@ app.post("/api/control", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Backend running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
